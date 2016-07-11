555c18cb7af306aea27d300bbaa4426e59a0da82
liveseasonal <matt@liveseasonal.com>
2016-07-11 23:57:21 +0000

    Linking up nav bar befor props

--- react_server/src/App.js
+++ react_server/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
 
 export default class App extends Component {
 
   constructor(props) {
-    console.log('hello ;)');
+    console.log('hello');
     super(props);
   }
 
   render() {
     return (
-      <h1> Welcome to react baby! </h1>
+      <div>
+         <Nav/>
+         <h1> Welcome to react baby! </h1>
+      </div>
+      
     );
   }
 
