01f72dd3e0c599c94b7ad0dd83bc15bc9faa2560
David Begert <dbegert@usc.edu>
2016-07-11 12:51:24 -0700

    webpack.config.js file added.

---
+++ react_server/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:6000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    path.resolve(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+        loaders: ['react-hot','babel'], // The module to load. "babel" is short for "babel-loader"
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+module.exports = config;
