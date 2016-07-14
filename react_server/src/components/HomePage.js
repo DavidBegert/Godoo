460088991e8ad965b88d4c2705a786c4cc765988
terencech <terence000@gmail.com>
2016-07-13 17:06:13 -0700

    added static empty components to home page

--- react_server/src/components/HomePage.js
+++ react_server/src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Hero from './Hero';
+import InfoSection from './InfoSection';
 
 export default class HomePage extends Component {
+
   render() {
     return(
-      <div>
-      </div>
+      <Hero />
+      <InfoSection />
     );
   }
+
 }
---
+++ react_server/src/components/SearchForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchForm extends Component {
+
+  render() {
+    return (
+      <input type="text" placeholder="Enter a city"></input>
+    );
+  }
+
+}
