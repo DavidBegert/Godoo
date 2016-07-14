10e0ecd5623c4f5e8f532a1c040222b82df30928
tristan <tsmackinlay@gmail.com>
2016-07-14 15:53:10 -0700

    stripped html from descriptions on event cards and marker popups with jquery

--- react_server/src/components/EventList.js
+++ react_server/src/components/EventList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import EventCard from './EventCard';
+import $ from 'jquery';
 
 export default class EventList extends Component {
 
@@ -11,11 +12,12 @@ export default class EventList extends Component {
 
     if (this.props.selectedEvents) {
       var eventCards = this.props.selectedEvents.map(function(event) {
+        var description = event.description ? $(event.description).text() : "No description.";
         return (
           <EventCard 
             key={event.id}
             title={event.title}
-            description={event.description ? event.description.slice(0, 300) + '...' : "No description."}
+            description={description.length > 300 ? description.slice(0, 300) + '...' : description}
             start_time={event.start_time}
             venue_name={event.venue_name}
             venue_address={event.venue_address}
