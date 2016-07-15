7ad6d53956e80e8480e4d801594c4ac427e752ae
David Begert <dbegert@usc.edu>
2016-07-15 13:27:05 -0700

    gave search input field styles

--- react_server/src/components/SearchForm.js
+++ react_server/src/components/SearchForm.js
@@ -15,27 +15,25 @@ export default class SearchForm extends Component {
       bounds: defaultBounds
     });
   }
-  
-  static inputStyle = {
-    border: "1px solid transparent",
-    borderRadius: "1px",
-    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
-    boxSizing: "border-box",
-    MozBoxSizing: "border-box",
-    fontSize: "14px",
-    height: "32px",
-    marginTop: "27px",
-    outline: "none",
-    padding: "0 12px",
-    textOverflow: "ellipses",
-    width: "400px"
-  }
 
 
   render() {
-
+    var inputStyle = {
+      border: "1px solid transparent",
+      borderRadius: "1px",
+      boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
+      boxSizing: "border-box",
+      MozBoxSizing: "border-box",
+      fontSize: "14px",
+      height: "32px",
+      marginTop: "27px",
+      outline: "none",
+      padding: "0 12px",
+      textOverflow: "ellipses",
+      width: "400px"
+    }
     return (
-      <input id="searchTextField" placeholder="Enter your address" type="text"></input>
+      <input style={inputStyle} id="searchTextField" placeholder="Enter your address" type="text" onSubmit={console.log('hello')}></input>
     );
   }
 
