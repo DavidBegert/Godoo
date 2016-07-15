7c9f9e05b6284352de1d08905f3df036748bc5fb
David Begert <dbegert@usc.edu>
2016-07-15 12:50:47 -0700

    search form on homepage works powered by google maps api

--- react_server/src/components/HomePage.js
+++ react_server/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Hero from './Hero';
 import InfoSection from './InfoSection';
+import SearchForm from './SearchForm';
 
 export default class HomePage extends Component {
 
@@ -9,6 +10,7 @@ export default class HomePage extends Component {
       <div>
         <Hero />
         <InfoSection />
+        <SearchForm />
         <button onClick={this.props.switchPage}> Switch page </button>
       </div>
     );
--- react_server/src/components/SearchForm.js
+++ react_server/src/components/SearchForm.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
+
 export default class SearchForm extends Component {
 
+  componentDidMount() {
+   var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(28.70, -127.50), 
+    new google.maps.LatLng(48.85, -55.90)
+    );
+
+    var input = document.getElementById('searchTextField');
+
+    var searchBox = new google.maps.places.SearchBox(input, {
+      bounds: defaultBounds
+    });
+  }
+  
+  static inputStyle = {
+    border: "1px solid transparent",
+    borderRadius: "1px",
+    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.3)",
+    boxSizing: "border-box",
+    MozBoxSizing: "border-box",
+    fontSize: "14px",
+    height: "32px",
+    marginTop: "27px",
+    outline: "none",
+    padding: "0 12px",
+    textOverflow: "ellipses",
+    width: "400px"
+  }
+
+
   render() {
+
     return (
-      <input type="text" placeholder="Enter a city"></input>
+      //<input type="text" placeholder="Enter a city"></input>
+       /* <SearchBox
+          onPlacesChanged={this.handlePlacesChanged}
+          ref="searchBox"
+          placeholder="Customized your placeholder"
+          style={SearchForm.inputStyle}
+        />*/
+        <div id='diver'>
+          <input id="searchTextField" placeholder="Enter your address" type="text"></input>
+        </div>
+
     );
   }
 
