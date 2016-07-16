5936c79accdc1fba98cc7981b937ee0614d448a7
liveseasonal <matt@liveseasonal.com>
2016-07-16 19:35:44 +0000

    Added navbar to homepage

--- react_server/src/components/HomePage.js
+++ react_server/src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
 import Hero from './Hero';
 import InfoSection from './InfoSection';
 import SearchForm from './SearchForm';
@@ -11,6 +12,7 @@ export default class HomePage extends Component {
   render() {
     return(
       <div>
+        <Nav />
         <Hero />
         <InfoSection />
         <SearchForm makeCall={this.props.makeCall}/>
