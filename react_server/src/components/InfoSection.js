6b0d705b91611af24bb0a6c50804596ab5438e84
liveseasonal <matt@liveseasonal.com>
2016-07-16 04:09:03 +0000

    Added image folder and linked in images

--- react_server/src/components/InfoSection.js
+++ react_server/src/components/InfoSection.js
@@ -13,21 +13,21 @@ export default class InfoSection extends Component {
         <br />
           <div className="columns" id="how-it-works-images">
             <div className="column is-4 ">
-              <img src="phone_in_the_country.jpg"/>
+              <img src="/src/images/phone_in_the_country copy.jpg"/>
               <br />
               <p><strong>Browse Events</strong></p>
               <br />
               <p> We bring together a variety of events recommended by industry experts</p>
             </div>
             <div className="column is-4">
-              <img src="man_photos_city.jpeg"/>
+              <img src="/src/images/man_photos_city copy.jpeg"/>
               <br />
               <p><strong>Create an Itinerary</strong></p>
               <br />
               <p> Select your favourite events and create an itinerary for you and your friends</p>
             </div>
             <div className="column is-4">
-              <img src="on_the_beach.jpg"/>
+              <img src="/src/images/on_the_beach copy.jpg"/>
               <br />
               <p><strong>Enjoy your experience</strong></p>
               <br />
