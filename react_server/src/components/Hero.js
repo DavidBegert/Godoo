3cff07aac687068ca557544ff5935bf4beaf12ef
liveseasonal <matt@liveseasonal.com>
2016-07-16 02:10:25 +0000

    class changes to className

--- react_server/src/components/Hero.js
+++ react_server/src/components/Hero.js
@@ -5,16 +5,16 @@ export default class Hero extends Component {
   render() {
     return (
       <section className="hero is-large">
-        <div class="hero-body hero-pic">
-         <div class="container city-heading">
-          <h1 class="title hero-title">
+        <div className="hero-body hero-pic">
+         <div className="container city-heading">
+          <h1 className="title hero-title">
             Discover events around the city
             </h1>
             <br />
             <br />
-            <input class="input input-city" type="text" placeholder="Enter a city"></input>
-            <input class="input input-date" type="date" placeholder="Pick a date"></input>
-            <button class="button get-started">
+            <input className="input input-city" type="text" placeholder="Enter a city"></input>
+            <input className="input input-date" type="date" placeholder="Pick a date"></input>
+            <button className="button get-started">
               Get Started
             </button>
            </div>
