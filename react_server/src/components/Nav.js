897d0c2d99c549dc5f9904623eb1248460501130
liveseasonal <matt@liveseasonal.com>
2016-07-16 02:33:02 +0000

    Added updated nav bar

--- react_server/src/components/Nav.js
+++ react_server/src/components/Nav.js
@@ -6,56 +6,22 @@ export default class Nav extends Component {
     return (
       <nav className="nav">
         <div className="nav-left">
-          <a className="nav-item" href="#">
-            <img className="logo" src="images/godoo_pre-04.jpg" alt="Godoo" />
-          </a>
-          <a className="nav-item" href="#">
-            <input type="text" placeholder="Vancouver"></input>
+          <a className="nav-item logo" href="#">
+            <strong>GoDoo</strong>
           </a>
         </div>
-
-        <div className="nav-center">
-          <a className="nav-item" href="#">
-            <span className="icon">
-              <i className="fa fa-github"></i>
-            </span>
-          </a>
-          <a className="nav-item" href="#">
-            <span className="icon">
-              <i className="fa fa-twitter"></i>
-            </span>
-          </a>
-        </div>
-
-        <span className="nav-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
 
         <div className="nav-right nav-menu">
           <a className="nav-item" href="#">
-            Home
-          </a>
-          <a className="nav-item" href="#">
-            Documentation
+            Explore
           </a>
           <a className="nav-item" href="#">
             Blog
           </a>
 
           <span className="nav-item">
-            <a className="button" >
-              <span className="icon">
-                <i className="fa fa-twitter"></i>
-              </span>
-              <span>Tweet</span>
-            </a>
             <a className="button is-primary" href="#">
-              <span className="icon">
-                <i className="fa fa-download"></i>
-              </span>
-              <span>Download</span>
+              <span>Contact us</span>
             </a>
           </span>
         </div>
